1b93f910f141c32940433ce3ce5ed3b533ef138b
HackSmith010 <anshujaiswal119@gmail.com>
2025-09-27 11:53:21 +0530

    changes in the model

--- backend/src/database/initDB.js
+++ backend/src/database/initDB.js
@@ -1,46 +1,43 @@
 import pool from "./db.js";
 import { createEnums } from "../models/enum.model.js";
-import { createUserTable } from "../models/users.model.js";
+import { createUserTable } from "../models/user.model.js";
 import { createDonorTable } from "../models/donor.model.js";
-import { createInstitutionTable } from "../models/institute.model.js";    
-import { createDonationTable } from "../models/donation.model.js";
-import { createBlockchainEventTable } from "../models/blockchainEvent.model.js";
-import { createFeedbackTable } from "../models/feedback.model.js";
-import { createKycDocTable } from "../models/kycDoc.model.js";
-import { createLogisticsTable } from "../models/logistic.model.js";
-import { createRequestTable } from "../models/request.model.js";
+import { createInstitutionTable } from "../models/institution.model.js";
 import { createSupplierTable } from "../models/supplier.model.js";
+import { createKycDocTable } from "../models/kycDoc.model.js";
+import { createRequestTable } from "../models/request.model.js";
+import { createDonationTable } from "../models/donation.model.js";
+import { createLogisticsTable } from "../models/logistic.model.js";
+import { createFeedbackTable } from "../models/feedback.model.js";
+import { createBlockchainEventTable } from "../models/blockchainEvent.model.js";
 
 export const initDB = async () => {
-    console.log("Initializing database...");
+    console.log("🚀 Initializing database...");
     try {
-        
-        await createEnums(pool);
-        await createUserTable(pool);
-        await createBlockchainEventTable(pool)
-        await createDonationTable(pool);
-        await createDonorTable(pool);
-        await createFeedbackTable(pool);
-        await createInstitutionTable(pool);
-        await createKycDocTable(pool);
-        await createLogisticsTable(pool);
-        await createRequestTable(pool);
-        await createSupplierTable(pool);
+
+        await createEnums();
+
+        await createUserTable();
+
+        await createDonorTable();
+        await createInstitutionTable();
+        await createSupplierTable();
+        await createKycDocTable();
+
+        await createRequestTable();
+
+        await createDonationTable();
+
+        await createLogisticsTable();
+        await createFeedbackTable();
+        await createBlockchainEventTable();
 
         console.log("✅ Database initialized, all tables created.");
+
     } catch (err) {
         console.error("❌ Database initialization failed:", err.message);
+    } finally {
+        console.log("--> Closing database pool.");
+        await pool.end();
     }
 };
-
-process.on("SIGINT", async () => {
-    console.log("Closing database connection...");
-    await pool.end();
-    process.exit(0);
-});
-
-process.on("SIGTERM", async () => {
-    console.log("Closing database connection...");
-    await pool.end();
-    process.exit(0);
-});
